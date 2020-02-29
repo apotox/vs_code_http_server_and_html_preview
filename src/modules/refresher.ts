200d7ad0380f37465c798938ef37a7d82130d4d5
Safi eddine <saphidev@gmail.com>
2020-02-29 10:21:03 +0100

    Update refresher.ts
    get iframe title and update the document title

--- src/modules/refresher.ts
+++ src/modules/refresher.ts
@@ -5,21 +5,35 @@ export class Refresher {
 		return `
 		<iframe src="${mainFile}" id="shs_website" allowTransparency="false" frameborder="0"></iframe>
 		<script>
+
+			const getIframe = ()=>{
+				return document.getElementById("shs_website");
+			}
+
+			const updateDocumentTitle=()=>{
+				const iframe = getIframe();
+				if(iframe){
+					let currentTitle = iframe.contentDocument.title;
+					document.title = currentTitle;
+				}
+			}
+
 			const websocket = new WebSocket("ws://" + document.location.host);
-	
 			websocket.onmessage = (event) => {
 				if (event.data == "refresh") {
-					const iframe = document.getElementById("shs_website");
-	
+					
+					const iframe = getIframe()
 					const currScroll = iframe.contentWindow.document.scrollingElement.scrollTop;
 					
 					iframe.src = iframe.contentWindow.location.href;
 					
 					iframe.onload = _ => {
 						iframe.contentWindow.document.scrollingElement.scrollTop = currScroll;
+						updateDocumentTitle();
 					}
 				}
 			}
+			updateDocumentTitle();
 		</script>
 		<style>
 			body, iframe {
